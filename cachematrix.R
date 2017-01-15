e0452f3a38aa0a93de7e7d74e94e5e2f52a25fa0
JanetL <inportant9@hotmail.com>
2017-01-15 22:37:10 +1100

    Week 3 Assignment JanetL

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This assignment is aiming to create a function cache inverse of a matrix 
 
-## Write a short comment describing this function
+# The first function creates a special "matrix" object that can cache its inverse. 
+makeCacheMatrix <- function(x = matrix()) { 
+        inv <- NULL 
+        set <- function(y) { 
+                x <<- y 
+                inv <<- NULL 
+        } 
+        get <- function() x 
+        setinverse <- function(inverse) inv <<- inverse 
+        getinverse <- function() inv 
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
+} 
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+# The following function returns the inverse of the matrix. 
+# It first checks if the inverse has already been computed. 
+# Yes = get the result and skips the computation. 
+# No = compute the inverse, sets the value in the cache using setinverse function. 
 
+# This function assumes that the matrix is always invertible. 
+cacheSolve <- function(x, ...) { 
+        inv <- x$getinverse() 
+        if(!is.null(inv)) { 
+                # get cached data given inverse matrix already exist 
+                return(inv) 
+        } 
+        data <- x$get() 
+        inv <- solve(data) 
+        x$setinverse(inv) 
+        inv 
+} 
 
-## Write a short comment describing this function
+# Result Output 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+> my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2)) 
+> my_matrix$get() 
+     [,1] [,2] 
+[1,]    1    3 
+[2,]    2    4 
+> my_matrix$getinverse() 
+NULL 
+> cacheSolve(my_matrix) 
+     [,1] [,2] 
+[1,]   -2  1.5 
+[2,]    1 -0.5 
+> my_matrix$getinverse() 
+     [,1] [,2] 
+[1,]   -2  1.5 
+[2,]    1 -0.5 
+> my_matrix$set(matrix(c(2,7,1,4),2,2)) 
+> my_matrix$get() 
+     [,1] [,2] 
+[1,]    2    1 
+[2,]    7    4 
+> my_matrix$getinverse() 
+NULL 
+> cacheSolve(my_matrix) 
+     [,1] [,2] 
+[1,]    4   -1 
+[2,]   -7    2
